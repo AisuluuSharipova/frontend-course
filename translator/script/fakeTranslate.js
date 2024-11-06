caca3906914a3c63bf5da1f90c6f47e38c275300
Айсулуу Шарипова <sharipovaa2507@gmail.com>
2024-11-07 00:26:47 +0600

    Tranlator done

---
+++ translator/script/fakeTranslate.js
@@ -0,0 +1,37 @@
+// Объект dictionary для хранения переводов
+const dictionary = {
+    "hello": "здравствуйте",
+    "world": "мир",
+    "friend": "друг",
+    "good": "хорошо",
+    "day": "день",
+    "night": "ночь",
+    "love": "любовь",
+    "peace": "мир",
+    "cat": "кот",
+    "dog": "собака",
+    "water": "вода",
+    "fire": "огонь",
+    "earth": "земля",
+    "wind": "ветер",
+    "sun": "солнце",
+    "moon": "луна",
+    "tree": "дерево",
+    "flower": "цветок",
+    "book": "книга",
+    "computer": "компьютер"
+};
+
+export function fakeTranslate(word) {
+    return new Promise((resolve, reject) => {
+        // Имитация асинхронной операции с задержкой
+
+        setTimeout(() => {
+            if (dictionary[word.toLowerCase()]) {
+                resolve(dictionary[word.toLowerCase()]);
+            } else {
+                reject("Перевод не найден.");
+            }
+        }, 1000); // Задержка 1 секунда
+    });
+}
