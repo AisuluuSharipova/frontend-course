0140706d7ec60d1faa6e34580ab306224083bbea
Aisuluu <sharipovaa2507@gmail.com>
2024-11-21 16:01:18 +0600

    some bugs are fixed

---
+++ frontend-course-react-proj/react-project/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import PersonInfo from './PersonInfo';
+
+const App = () => {
+    return (
+        <div>
+            <PersonInfo />
+        </div>
+    );
+};
+
+export default App;
+
