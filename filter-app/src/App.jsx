f3a189ea9c3c9466d010ee9d3616beb51eb525f4
Aisuluu <sharipovaa2507@gmail.com>
2024-11-27 03:15:29 +0600

    Style and more books are added

--- filter-app/src/App.jsx
+++ filter-app/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   useEffect(() => {
     const fetchData = async () => {
       const mockData = [
-        { id: 1, title: 'War and Peace' },
+        { id: 1, title: 'The Broken Sword' },
         { id: 2, title: 'Pride and Prejudice' },
         { id: 3, title: '1984' },
         { id: 4, title: 'The Catcher in the Rye' },
@@ -16,7 +16,17 @@ function App() {
         { id: 7, title: 'To Kill a Mockingbird' },
         { id: 8, title: 'Ulysses' },
         { id: 9, title: 'The Odyssey' },
-        { id: 10, title: 'Crime and Punishment' },
+        { id: 10, title: 'War and Peace' },
+        { id: 11, title: 'Crime and Punishment' },
+        { id: 12, title: 'Harry Potter' },
+        { id: 13, title: 'The Alchemist' },
+        { id: 14, title: 'The Hobbit' },
+        { id: 15, title: 'Brave New World' },
+        { id: 16, title: 'The Picture of Dorian Gray' },
+        { id: 17, title: 'Dracula' },
+        { id: 18, title: 'Frankenstein' },
+        { id: 19, title: 'The Brothers Karamazov' },
+        { id: 20, title: 'The Little Prince' },
       ];
       setData(mockData); 
     };
